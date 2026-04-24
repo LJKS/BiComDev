ad08be0503877549f1e97c4fef3334bc2695ce07
LJKS <Leon-Schmid@gmx.de>
2026-04-24 23:36:51 +0200

    fixed runscripts

--- run_confirmation_base.sh
+++ run_confirmation_base.sh
@@ -15,4 +15,4 @@ spack load cudnn@8.6.0.163-11.8
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=/appl/spack/opt/spack/linux-rocky8-zen/gcc-8.5.0/cuda-11.8.0-x32erfzo6xl2qgbp5enezl53wwiingmt
 export CUDA_DIR=/appl/spack/opt/spack/linux-rocky8-zen/gcc-8.5.0/cuda-11.8.0-x32erfzo6xl2qgbp5enezl53wwiingmt
 source avtivate Bidirectional_CommGame
-python ppo.py --num_same 2 --num_diff1 2 --num_diff2 2 --msg_len 1 --vocab_size 20  --num_steps 20 --num_epochs 8000 --embed_dim 48 --batch_size 128 --lstm_units 96
+python python $HOME/experiments/BiComDev/ppo.py --num_same 2 --num_diff1 2 --num_diff2 2 --msg_len 1 --vocab_size 20  --num_steps 20 --num_epochs 8000 --embed_dim 48 --batch_size 128 --lstm_units 96
